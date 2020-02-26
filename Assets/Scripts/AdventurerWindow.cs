96f12dfdfe16e4f5e2937e3cb1199a8ab965f770
lucas schraier <lucasfschraier@gmail.com>
2020-02-26 15:42:57 -0500

    Define SetAdventurer method to update sprite/text components with adventurer properties

---
+++ Assets/Scripts/AdventurerWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AdventurerWindow : ControlledWindow
+{
+    public SpriteRenderer spriteRenderer;
+    public TextMeshProUGUI nameText, bioText;
+
+    public void SetAdventurer(Adventurer adventurer)
+    {
+        spriteRenderer.sprite = adventurer.sprite;
+        nameText.text = adventurer.name;
+        bioText.text = $"Level {adventurer.level} {adventurer.race} {adventurer.adventurerClass}";
+    }
+
+}
