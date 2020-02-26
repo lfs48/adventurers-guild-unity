4465ced512149bc03d1878f1741b64c068273824
lucas schraier <lucasfschraier@gmail.com>
2020-02-26 14:56:38 -0500

    Implement hiding/showing window, hide on start

---
+++ Assets/Scripts/ControlledWindow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlledWindow : MonoBehaviour
+{
+    private RectTransform rect;
+    void Awake() {
+        rect = GetComponent<RectTransform>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideWindow();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowWindow()
+    {
+        rect.localScale = new Vector3(1, 1, 1);
+    }
+
+    public void HideWindow()
+    {
+        rect.localScale = new Vector3(0,0,0);
+    }
+}
