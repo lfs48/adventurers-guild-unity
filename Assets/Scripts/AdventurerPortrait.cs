65e7ba765506f6b7ff07f02d8a11385528a3c29e
lucas schraier <lucasfschraier@gmail.com>
2020-02-26 01:01:06 -0500

    Add text to display adventurer name

--- Assets/Scripts/AdventurerPortrait.cs
+++ Assets/Scripts/AdventurerPortrait.cs
@@ -8,9 +8,12 @@ public class AdventurerPortrait : MonoBehaviour
 {
     public Adventurer adventurer;
     public SpriteRenderer renderer;
+    public TextMeshProUGUI nameText;
+    
     // Start is called before the first frame update
     void Start()
     {
+        nameText.text = adventurer.name;
         renderer.sprite = adventurer.sprite;
     }
 
