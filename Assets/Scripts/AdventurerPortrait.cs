5073d13cc52eed70456ee7aa325f0e14f784f28c
lucas schraier <lucasfschraier@gmail.com>
2020-02-26 14:55:56 -0500

    Handle clicking portrait by opening adventurer info window

--- Assets/Scripts/AdventurerPortrait.cs
+++ Assets/Scripts/AdventurerPortrait.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class AdventurerPortrait : MonoBehaviour
+public class AdventurerPortrait : MonoBehaviour, IPointerDownHandler
 {
     public Adventurer adventurer;
     public SpriteRenderer renderer;
     public TextMeshProUGUI nameText;
+    public WindowController windowController;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        windowController.setAdventurer(adventurer);
+    }
     
     // Start is called before the first frame update
     void Start()
     {
+        windowController = GameObject.Find("WindowController").GetComponent<WindowController>();
         nameText.text = adventurer.name;
         renderer.sprite = adventurer.sprite;
     }
