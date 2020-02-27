96371f7e6c45e7b86dec6f6a9297c44813e2614b
lucas schraier <lucasfschraier@gmail.com>
2020-02-26 20:20:41 -0500

    Handle initialization of lists of class and racial traits, random assignment of traits from lists

---
+++ Assets/Scripts/TraitManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraitManager : MonoBehaviour
+{
+
+    public List<Trait> humanTraits;
+    public List<Trait> elfTraits;
+    public List<Trait> dwarfTraits;
+    public List<Trait> gnomeTraits;
+    public List<Trait> orcTraits;
+    public List<Trait> druidTraits;
+    public List<Trait> mageTraits;
+    public List<Trait> priestTraits;
+    public List<Trait> rogueTraits;
+    public List<Trait> warriorTraits;
+    private Dictionary<Race, List<Trait>> raceTraits;
+    private Dictionary<AdventurerClass, List<Trait>> classTraits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeClassTraits();
+        InitializeRaceTraits();
+    }
+
+    void InitializeRaceTraits() 
+    {
+        raceTraits = new Dictionary<Race, List<Trait>>();
+        raceTraits[Race.Human] = humanTraits;
+        raceTraits[Race.Elf] = elfTraits;
+        raceTraits[Race.Dwarf] = dwarfTraits;
+        raceTraits[Race.Gnome] = gnomeTraits;
+        raceTraits[Race.Orc] = orcTraits;
+    }
+
+    void InitializeClassTraits()
+    {
+        classTraits = new Dictionary<AdventurerClass, List<Trait>>();
+        classTraits[AdventurerClass.Druid] = druidTraits;
+        classTraits[AdventurerClass.Mage] = mageTraits;
+        classTraits[AdventurerClass.Priest] = priestTraits;
+        classTraits[AdventurerClass.Rogue] = rogueTraits;
+        classTraits[AdventurerClass.Warrior] = warriorTraits;
+    }
+
+    // Update is called once per frame
+    public Trait RandomRacialTrait(Race race)
+    {
+        return raceTraits[race][Random.Range(0, raceTraits[race].Count)];
+    }
+
+    public Trait RandomClassTrait(AdventurerClass advClass)
+    {
+        return classTraits[advClass][Random.Range(0, classTraits[advClass].Count)];
+    }
+}
