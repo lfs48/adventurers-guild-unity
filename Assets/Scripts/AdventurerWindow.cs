3377cc85dbb505e29f3e9e3cc20b225683d0fed1
lucas schraier <lucasfschraier@gmail.com>
2020-02-26 20:23:45 -0500

    Set text components to display adventurer traits

--- Assets/Scripts/AdventurerWindow.cs
+++ Assets/Scripts/AdventurerWindow.cs
@@ -6,13 +6,34 @@ using TMPro;
 public class AdventurerWindow : ControlledWindow
 {
     public SpriteRenderer spriteRenderer;
-    public TextMeshProUGUI nameText, bioText;
+    public TextMeshProUGUI nameText, bioText, raceTraitsText, classTraitsText;
 
     public void SetAdventurer(Adventurer adventurer)
     {
         spriteRenderer.sprite = adventurer.sprite;
         nameText.text = adventurer.name;
         bioText.text = $"Level {adventurer.level} {adventurer.race} {adventurer.adventurerClass}";
+        raceTraitsText.text = "";
+        classTraitsText.text = "";
+        Trait trait;
+        for (int i = 0; i < adventurer.traits.Count; i++)
+        {
+            trait = adventurer.traits[i];
+            switch(trait.source) 
+            {
+                case("Race"): 
+                {
+                    raceTraitsText.text += $"\n {trait.name}: {trait.description}";
+                    break;
+                }
+                case("Class"):
+                {
+                    classTraitsText.text += $"\n {trait.name}: {trait.description}";
+                    break;
+                }
+                default: break;
+            }
+        }
     }
 
 }
