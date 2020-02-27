d2ec28e2fc80a2eb38e5c02768833263121ed490
lucas schraier <lucasfschraier@gmail.com>
2020-02-27 11:57:48 -0500

    Revert to previous commit

--- Assets/Controls/CameraController.cs
+++ Assets/Controls/CameraController.cs
@@ -5,54 +5,44 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float panSpeed, panBorderThickness, scrollLimitMin, scrollLimitMax;
+    private float panSpeed, panBorderThickness;
     private Vector2 panLimit;
-    private Vector3 scrollSpeed;
-    public Transform mapTransform;
+    private Transform transform;
 
     void Awake()
     {
-        panSpeed = 10000f;
-        panBorderThickness = 150f;
+        panSpeed = 2f;
+        panBorderThickness = 200f;
         panLimit = new Vector2(9f, 5f);
-        scrollSpeed = new Vector3(1f, 1f, 1f);
-        scrollLimitMax = 0.02f;
-        scrollLimitMin = .005f;
+        transform = GetComponent<Transform>();
     }
     void Update()
     {
-        Vector3 pos = mapTransform.position;
-        Vector3 scale = mapTransform.localScale;
+        Vector3 pos = transform.position;
 
         if (Input.GetKey("w") || ( (Input.mousePosition.y >= Screen.height - panBorderThickness) && Input.mousePosition.y < Screen.height) )
         {
-            pos.y -= panSpeed / Mathf.Max(10, (Screen.height - Input.mousePosition.y ) ) * Time.deltaTime * scale.y;
+            pos.y += panSpeed * 200 / Mathf.Max(10, (Screen.height - Input.mousePosition.y ) ) * Time.deltaTime;
         }
 
         if (Input.GetKey("a")  || ( (Input.mousePosition.x <= panBorderThickness) && Input.mousePosition.x > 0) )
         {
-            pos.x += panSpeed / Mathf.Max(10, (Input.mousePosition.x) ) * Time.deltaTime * scale.x;
+            pos.x -= panSpeed * 200 / Mathf.Max(10, (Input.mousePosition.x) ) * Time.deltaTime;
         }
 
         if (Input.GetKey("s")  || ( (Input.mousePosition.y <= panBorderThickness) && Input.mousePosition.y > 0) )
         {
-            pos.y += panSpeed / Mathf.Max(10, (Input.mousePosition.y) ) * Time.deltaTime * scale.y;
+            pos.y -= panSpeed * 200 / Mathf.Max(10, (Input.mousePosition.y) ) * Time.deltaTime;
         }
 
         if (Input.GetKey("d")  || ( (Input.mousePosition.x >= Screen.width - panBorderThickness) && Input.mousePosition.x < Screen.width) )
         {
-            pos.x -= panSpeed / Mathf.Max(10, (Screen.width - Input.mousePosition.x ) ) * Time.deltaTime * scale.x;
+            pos.x += panSpeed * 200 / Mathf.Max(10, (Screen.width - Input.mousePosition.x ) ) * Time.deltaTime;
         }
-
-        scale += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime;
 
         pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
         pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
-        scale.x = Mathf.Clamp(scale.x, scrollLimitMin, scrollLimitMax);
-        scale.y = Mathf.Clamp(scale.y, scrollLimitMin, scrollLimitMax);
-        scale.z = Mathf.Clamp(scale.z, scrollLimitMin, scrollLimitMax);
 
-        mapTransform.position = pos;
-        mapTransform.localScale = scale;
+        transform.position = pos;
     }
 }
