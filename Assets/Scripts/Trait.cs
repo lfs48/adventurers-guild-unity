f470ca0aff4dc98422e2b5342799d2be19bfd0e1
lucas schraier <lucasfschraier@gmail.com>
2020-02-26 20:24:08 -0500

    Add source field

--- Assets/Scripts/Trait.cs
+++ Assets/Scripts/Trait.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Trait", menuName = "Trait")]
 public class Trait : ScriptableObject
 {
-    public string name, description;
+    public string name, description, source;
     public int value;
 }
