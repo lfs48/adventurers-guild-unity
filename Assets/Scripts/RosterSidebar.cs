d3b8358d604114bbed529dc7c608c9c37868da24
lucas schraier <lucasfschraier@gmail.com>
2020-02-26 20:22:33 -0500

    Handle adding new adventurers to roster

--- Assets/Scripts/RosterSidebar.cs
+++ Assets/Scripts/RosterSidebar.cs
@@ -7,6 +7,7 @@ public class RosterSidebar : MonoBehaviour
     public Player player;
     public AdventurerPortrait advPortraitPrefab;
     private Transform tf;
+    private List<Adventurer> roster;
     // Start is called before the first frame update
 
     void Awake()
@@ -15,13 +16,13 @@ public class RosterSidebar : MonoBehaviour
     }
     void Start()
     {
-        setupRoster();
+        SetupRoster();
     }
 
-    void setupRoster()
+    void SetupRoster()
     {
-        Adventurer[] roster = player.roster;
-        for (int i = 0; i < roster.Length; i++)
+        roster = player.roster;
+        for (int i = 0; i < roster.Count; i++)
         {
             AdventurerPortrait portrait = Instantiate(advPortraitPrefab, tf);
             portrait.adventurer = roster[i];
@@ -29,9 +30,17 @@ public class RosterSidebar : MonoBehaviour
         }
     }
 
+    public void AddAdventurerToRoster(Adventurer adventurer)
+    {
+        AdventurerPortrait portrait = Instantiate(advPortraitPrefab, tf);
+        portrait.adventurer = adventurer;
+        portrait.transform.position = new Vector3(-6.271f,3.269f - ( roster.Count *0.978f) , 0);
+        roster.Add(adventurer);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
