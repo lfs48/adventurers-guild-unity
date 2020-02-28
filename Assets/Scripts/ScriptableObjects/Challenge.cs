829663f80116eb42091bb0d5f1bc2ea94e4f6e19
lucas schraier <lucasfschraier@gmail.com>
2020-02-27 19:06:53 -0500

    Initial commit

---
+++ Assets/Scripts/ScriptableObjects/Challenge.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Challenge : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public int value;
+    public List<Trait> relevantTraits;
+}
