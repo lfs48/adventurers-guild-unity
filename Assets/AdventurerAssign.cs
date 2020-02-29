742184c11ccb08823d89edfac48f5354efc312e3
lucas schraier <lucasfschraier@gmail.com>
2020-02-29 00:27:55 -0500

    Handle assigning adventurers from roster to embark on quest

---
+++ Assets/AdventurerAssign.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventurerAssign : MonoBehaviour
+{
+    private Adventurer[] adventurers;
+    public List<BoxCollider2D> boxes;
+    public List<Image> advSprites;
+    public BoxCollider2D mainBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        adventurers = new Adventurer[4];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AssignAdventurer(Adventurer adventurer, Vector3 mousePos)
+    {
+        
+        for (int i = 0; i < boxes.Count; i++)
+        {
+            if (boxes[i].OverlapPoint(mousePos)) 
+            {
+                int idx = System.Array.IndexOf(adventurers, adventurer);
+                if (idx >= 0) 
+                { 
+                    if (adventurers[i] is null) 
+                    {
+                        adventurers[idx] = null;
+                    } else {
+                        adventurers[idx] = adventurers[i]; 
+                    }
+                }
+                adventurers[i] = adventurer;
+                break;
+            }
+        }
+
+        for (int i = 0; i < adventurers.Length; i++)
+        {
+            if (!(adventurers[i] is null)) 
+            { 
+                advSprites[i].sprite = adventurers[i].portrait; 
+            } else
+            {
+                advSprites[i].sprite = null;
+            }
+        }
+    }
+}
