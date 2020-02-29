4744dad23cd949326607394b1df7a0f19d1ad6f7
lucas schraier <lucasfschraier@gmail.com>
2020-02-29 16:29:27 -0500

    Delegate image rendering to AdventurerAssignBox components, rename to AdventurerAssignManager and move to Scrips/UI folder

--- Assets/Scripts/UI/AdventurerAssignManager.cs
+++ Assets/Scripts/UI/AdventurerAssignManager.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AdventurerAssign : MonoBehaviour
+public class AdventurerAssignManager : MonoBehaviour
 {
+    public List<AdventurerAssignBox> boxes;
     private Adventurer[] adventurers;
-    public List<BoxCollider2D> boxes;
-    public List<Image> advSprites;
     public BoxCollider2D mainBox;
     // Start is called before the first frame update
     void Start()
@@ -26,7 +25,7 @@ public class AdventurerAssign : MonoBehaviour
         
         for (int i = 0; i < boxes.Count; i++)
         {
-            if (boxes[i].OverlapPoint(mousePos)) 
+            if (boxes[i].collision.OverlapPoint(mousePos)) 
             {
                 int idx = System.Array.IndexOf(adventurers, adventurer);
                 if (idx >= 0) 
@@ -34,24 +33,24 @@ public class AdventurerAssign : MonoBehaviour
                     if (adventurers[i] is null) 
                     {
                         adventurers[idx] = null;
+                        boxes[idx].ClearAdventurer();
                     } else {
                         adventurers[idx] = adventurers[i]; 
+                        boxes[idx].SetAdventurer(adventurers[idx]);
                     }
                 }
                 adventurers[i] = adventurer;
+                boxes[i].SetAdventurer(adventurer);
                 break;
             }
         }
 
-        for (int i = 0; i < adventurers.Length; i++)
-        {
-            if (!(adventurers[i] is null)) 
-            { 
-                advSprites[i].sprite = adventurers[i].portrait; 
-            } else
-            {
-                advSprites[i].sprite = null;
-            }
-        }
+    }
+
+    public void ClearAdventurer(Adventurer adventurer)
+    {
+        int i = System.Array.IndexOf(adventurers, adventurer);
+        adventurers[i] = null;
+        boxes[i].ClearAdventurer();
     }
 }
