03603977789e83ffb065c76224afd70eee841142
lucas schraier <lucasfschraier@gmail.com>
2020-02-29 20:00:34 -0500

    Add state field, define QuestState enum

--- Assets/Scripts/ScriptableObjects/Quest.cs
+++ Assets/Scripts/ScriptableObjects/Quest.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum QuestState
+{
+    Available, Active, Completed
+}
+
 [CreateAssetMenu(fileName = "New Quest", menuName = "Quest")]
 public class Quest : ScriptableObject
 {
     public new string name;
     public string description;
     public List<Challenge> challenges;
+    public QuestState state;
 }
