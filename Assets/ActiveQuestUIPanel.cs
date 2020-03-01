11f9caf4145f5618576665b9f7910e6469632a28
lucas schraier <lucasfschraier@gmail.com>
2020-02-29 23:25:14 -0500

    Set progress bar value and quest state message on update

---
+++ Assets/ActiveQuestUIPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ActiveQuestUIPanel : QuestUIPanel
+{
+    private ActiveQuest quest;
+    public Slider progress;
+    public TextMeshProUGUI questStateText;
+    void Update()
+    {
+        if (!(quest is null)) 
+        {
+            progress.value = quest.GetProgress();
+            questStateText.text = quest.GetMessage();
+        }
+    }
+
+    public void SetQuest(ActiveQuest quest)
+    {
+        this.quest = quest;
+    }
+}
+
