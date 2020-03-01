7d199d3a78d7c165e604d76a2c3824eee26cf076
lucas schraier <lucasfschraier@gmail.com>
2020-02-29 21:53:15 -0500

    Implement Show and Hide methods to display quest panel UI info

---
+++ Assets/Scripts/UI/QuestUIPanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuestUIPanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RectTransform rect;
+    public TextMeshProUGUI nameText, descText;
+
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    public void Show(Quest quest)
+    {
+        rect.localScale = new Vector3(1,1,1);
+        nameText.text = quest.name;
+        descText.text = quest.description;
+    }
+
+    public void Hide() 
+    {
+        rect.localScale = new Vector3(0,0,0);
+    }
+}
