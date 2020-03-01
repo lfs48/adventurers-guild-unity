e99d920d96c68abf86b46dbbb59ba9fcc54465a4
lucas schraier <lucasfschraier@gmail.com>
2020-02-29 23:22:49 -0500

    Implement quest state that changes based on challenge completion/failure, add GetMessage method to display UI messages based on quest state

--- Assets/Scripts/ActiveQuest.cs
+++ Assets/Scripts/ActiveQuest.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ActiveQuestState
+{
+    Embarking, AttemptingChallenge, DefeatedChallenge, FailedChallenge, Success
+}
+
 public class ActiveQuest : MonoBehaviour
 {
     public QuestManager manager;
@@ -10,6 +15,7 @@ public class ActiveQuest : MonoBehaviour
     private List<Trait> assignedTraits;
     private float timer, waitTime;
     private int challengeIndex;
+    private ActiveQuestState state;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,34 +28,58 @@ public class ActiveQuest : MonoBehaviour
         timer -= Time.deltaTime;
         if (timer <= 0)
         {
-            bool success = ResolveChallenge(quest.challenges[challengeIndex]);
-            timer = waitTime;
-            if (success)
+            switch(state) 
             {
-                challengeIndex++;
-                if (challengeIndex >= quest.challenges.Count) {
-                    CompleteQuest();
-                } else {
-                    Debug.Log("Challenge Defeated");
-                }
-            } else {
-                Debug.Log("Failed Challenge");
+                case ActiveQuestState.Embarking: 
+                case ActiveQuestState.DefeatedChallenge: 
+                case ActiveQuestState.FailedChallenge: 
+                    BeginNextChallenge();
+                    break;
+                case ActiveQuestState.AttemptingChallenge: 
+                    bool success = ResolveChallenge(quest.challenges[challengeIndex]);
+                    if (success && challengeIndex >= quest.challenges.Count - 1) {CompleteQuest(); }
+                    else if (success) {SucceedChallenge(); }
+                    else { FailChallenge(); }
+                    break;
+                case ActiveQuestState.Success: 
+                    EndQuest();
+                    break;
             }
         }
     }
 
     public void Initialize(QuestManager manager, Quest quest, Adventurer[] adventurers)
     {
+        state = ActiveQuestState.Embarking;
         this.manager = manager;
         this.quest = quest;
         this.adventurers = adventurers;
-        waitTime = 2f;
+        waitTime = 4f;
         timer = waitTime;
         assignedTraits = new List<Trait>();
         for (int i = 0; i < adventurers.Length; i++)
         {
             assignedTraits.AddRange(adventurers[i].traits);
         }
+    }
+
+    void BeginNextChallenge()
+    {
+        state = ActiveQuestState.AttemptingChallenge;
+        timer = waitTime;
+    }
+
+    void SucceedChallenge()
+    {
+        state = ActiveQuestState.DefeatedChallenge;
+        challengeIndex++;
+        timer = waitTime / 2;
+    }
+
+    void FailChallenge()
+    {
+        state = ActiveQuestState.FailedChallenge;
+        timer = waitTime / 2;
     }
 
     bool ResolveChallenge(Challenge challenge)
@@ -70,11 +100,34 @@ public class ActiveQuest : MonoBehaviour
 
     void CompleteQuest()
     {
-        challengeIndex = 0;
-        timer = waitTime;
-        Debug.Log("Quest Complete!");
+        state = ActiveQuestState.Success;
+        timer = waitTime / 2;
+        challengeIndex++;
         quest.state = QuestState.Completed;
+    }
+
+    void EndQuest()
+    {
         manager.CompleteQuest(this);
+    }
+
+    public float GetProgress()
+    {
+        return (float) (challengeIndex) / (float) quest.challenges.Count;
+    }
+
+    public string GetMessage()
+    {
+        switch(state)
+        {
+            case ActiveQuestState.Embarking: return "Embarking on quest...";
+            case ActiveQuestState.AttemptingChallenge: return $"Attempting challenge: {quest.challenges[challengeIndex].name}";
+            case ActiveQuestState.DefeatedChallenge: return $"Defeated challenge: {quest.challenges[challengeIndex - 1].name}";
+            case ActiveQuestState.FailedChallenge: return $"Failed challenge: {quest.challenges[challengeIndex].name}";
+            case ActiveQuestState.Success: return "Quest complete! Bringing home loot";
+
+            default: return "";
+        }
     }
 
 }
