fea804042c63a5c7670dd320babca0d4cba08b45
lucas schraier <lucasfschraier@gmail.com>
2020-03-05 13:17:00 -0500

    Implement tooltiptable interface that allows setting contents of a tooltip

--- Assets/Scripts/ScriptableObjects/Challenge.cs
+++ Assets/Scripts/ScriptableObjects/Challenge.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Challenge", menuName = "Challenge")]
-public class Challenge : ScriptableObject
+public class Challenge : ScriptableObject, Tooltipable
 {
     public new string name;
     public string description;
     public int value;
     public List<Trait> relevantTraits;
     public Sprite icon;
+
+    public string GetTooltipTitle()
+    {
+        return name;
+    }
+
+    public string GetTooltipDesc()
+    {
+        return description;
+    }
 }
--- Assets/Scripts/ScriptableObjects/Trait.cs
+++ Assets/Scripts/ScriptableObjects/Trait.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Trait", menuName = "Trait")]
-public class Trait : ScriptableObject
+public class Trait : ScriptableObject, Tooltipable
 {
     public string name, description, source;
     public int value;
     public Sprite icon;
+
+    public string GetTooltipTitle()
+    {
+        return name;
+    }
+
+    public string GetTooltipDesc()
+    {
+        return description;
+    }
 }
