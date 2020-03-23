79f9f1c78bc208ce38334994bccb90983f15fbfa
lucas schraier <lucasfschraier@gmail.com>
2020-03-23 15:53:00 -0400

    Update assets

--- Assets/Scripts/UI/MouseoverIcon.cs
+++ Assets/Scripts/UI/MouseoverIcon.cs
@@ -7,11 +7,14 @@ public class MouseoverIcon : MonoBehaviour
 {
     private Tooltipable tooltip;
     public Image icon;
+    public Canvas canvas;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.localScale = new Vector3(0,0,0);
+        GameObject temp = GameObject.Find("MainCanvas");
+        canvas = temp.GetComponent<Canvas>();
     }
 
     // Update is called once per frame
@@ -22,7 +25,7 @@ public class MouseoverIcon : MonoBehaviour
 
     public void OnMouseEnter()
     {
-        Vector3 pos = Camera.main.ScreenToWorldPoint( Camera.main.WorldToScreenPoint(transform.position)  + new Vector3(0f, -120f, 0f) );
+        Vector3 pos = Camera.main.ScreenToWorldPoint( Camera.main.WorldToScreenPoint(transform.position)  + new Vector3(0f, -50f / canvas.scaleFactor, 0f) );
         Tooltip.ShowTooltip(tooltip, pos);
     }
 
